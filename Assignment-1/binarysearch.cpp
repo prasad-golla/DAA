2bcd47e022c07c8c98c3386bc1fb1c4e36645083
Golla Prasad <prasad.golla2008@gmail.com>
2026-08-19 21:07:53 +0530

    Organized Assignment 1

---
+++ Assignment-1/binarysearch.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements in sorted order: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int key;
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            cout << "Element found at index " << mid;
+            return 0;
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    cout << "Element not found";
+
+    return 0;
+}
